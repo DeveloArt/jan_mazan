2f00547bf321770abe52b38785dad9769cab5e66
Arek Daszkowski <daszkowskiarek145@gmail.com>
2025-03-18 13:27:11 +0100

    add captcha

--- template-parts/contact/contact-form.php
+++ template-parts/contact/contact-form.php
@@ -1,3 +1,10 @@
 <div class="contact-form-section">
     <?php echo do_shortcode('[contact-form-7 id="bb9ac6b" title="Formularz 1"]'); ?>
 </div>
+
+<script>
+function turnstileCallback(token) {
+    document.getElementById('turnstile-token').value = token;
+}
+
+</script>
