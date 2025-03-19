fb77c92e9ae7f02115192829a7463eefafca7cb2
Adrianm7777 <Adrianmaterka10@gmail.com>
2025-03-19 17:40:57 +0100

    feature: update layout and styling for contact and added map sections

--- template-parts/contact/contact-text.php
+++ template-parts/contact/contact-text.php
@@ -4,7 +4,7 @@ if (function_exists('get_field')) :
     $description = get_field('contact_section_description');
     $button_text = get_field('contact_section_button_text');
     $button_url = get_field('contact_section_button_url');
-    $image_url = get_field('contact_section_image');
+    
 ?>
 
     <div class="contact-text">
@@ -15,9 +15,8 @@ if (function_exists('get_field')) :
             <li><span class="header-contact-title">E-MAIL:</span> <?php the_field('email_address'); ?></li>
         </ul>
         <p><?php echo esc_html($description); ?></p>
-        <?php if ($image_url): ?>
-            <img src="<?php echo esc_url($image_url); ?>" alt="<?php echo esc_attr($title); ?>">
-        <?php endif; ?>
+ <div class="map-section-map">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2348.7739513454612!2d20.9806371!3d52.220628999999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471ecca298a73673%3A0x4fb81391a02fa58d!2sKaliska%2017%2F38%2C%2002-316%20Warszawa!5e1!3m2!1spl!2spl!4v1742393569568!5m2!1spl!2spl" width="100%" height="100%" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>        </div>
         <?php render_custom_button(esc_html($button_text), false, esc_url($button_url), false); ?>
     </div>
 
